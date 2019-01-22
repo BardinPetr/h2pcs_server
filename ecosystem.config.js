60696ca5aa008b0b71e3b5257156400bcef4e2ed
Bardin Petr <bardin.petr@gmail.com>
2019-01-22 09:41:44 +0300

    Ecosystem added

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: "h2pcs",
+      script: "server.js",
+      env_production: {
+        AUTH0_CLIENT_ID: "SToP1tvfsjE1EckPC00G2n3uAqJdRi58",
+        AUTH0_DOMAIN: "deep-null.auth0.com",
+        AUTH0_CLIENT_SECRET:
+          "ZHlC1kQntwct-zMxFaieOsS1ONgY0dPIhX-ibplg_YwJnXFfmJcSoxIY27Cejyq7",
+        AUTH0_CALLBACK_URL: "http://localhost:3777/callback",
+        IS_RELEASE: "true",
+        PORT: 3777
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      key: "~/.ssh/id_rsa.pub",
+      user: "root",
+      host: ["212.109.194.80"],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      repo: "git@bitbucket.org:BardinPetr/sgh_server.git",
+      path: "/home/root/h2pcs",
+      "pre-setup": "",
+      "post-setup": "",
+      "pre-deploy-local": "echo 'Deploy finished'",
+      "post-deploy":
+        "npm install; pm2 startOrRestart ecosystem.config.js; pm2 restart ecosystem.config.js --env production --update-env"
+    }
+  }
+};
