ccf9a414a9b3c92d8f3d32de3c81c9c8f02e5c60
BardinPetr <bardin.petr@gmail.com>
2020-02-29 17:44:54 +0300

    Ecosystem fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         AUTH0_DOMAIN: "deep-null.auth0.com",
         AUTH0_CLIENT_SECRET:
           "ZHlC1kQntwct-zMxFaieOsS1ONgY0dPIhX-ibplg_YwJnXFfmJcSoxIY27Cejyq7",
-        AUTH0_CALLBACK_URL: "http://h2pcs.onthewifi.com/callback",
+        AUTH0_CALLBACK_URL: "https://h2pcs.xyz/callback",
         IS_RELEASE: "true",
         PORT: 3777
       }
@@ -17,12 +17,12 @@ module.exports = {
   deploy: {
     production: {
       key: "~/.ssh/id_rsa.pub",
-      user: "root",
-      host: ["212.109.194.80"],
+      user: "deploy",
+      host: ["212.109.197.144"],
       ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "git@github.com:BardinPetr/h2pcs_server.git",
-      path: "/home/root/h2pcs",
+      path: "/home/deploy/h2pcs",
       "pre-setup": "",
       "post-setup": "",
       "pre-deploy-local": "echo 'Deploy finished'",
